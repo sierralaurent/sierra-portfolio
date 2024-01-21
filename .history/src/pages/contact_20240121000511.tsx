3bd3c5fa425164e2778aca02f1738e87eb947f57
Sierra Laurent <sierraclaurent@gmail.com>
2024-01-21 00:09:00 -0800

    contact page update

--- .history/src/pages/contact_20240121000511.tsx
+++ .history/src/pages/contact_20240121000511.tsx
@@ -7,14 +7,14 @@ export default function Contact() {
   return (
     <>
     <NavBar /> 
-    <main className={`flex min-h-screen flex-col items-center m-32 text-black `}>
+    <main className={`flex min-h-screen flex-col items-center text-black`}>
       <div className={'flex min-w-screen flex-col gap-20 m-4 items-center'}>
         <h2 className={'text-3xl'}>Now that you've seen it all...</h2>
         <h1 className={'text-5xl'}>Let's chat!</h1>
         <h3 className={'text-xl'}>Feel free to contact me.</h3>
       </div>
 
-      <div className={'flex min-w-full flex-row m-24 p-10 justify-between'}>
+      <div className={'flex min-w-full flex-row m-4 p-10 justify-between'}>
       <Link href=''>
         <div className={'flex flex-row items-center gap-2 hover:text-Sage text-base'}>
             <Image
