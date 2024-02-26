5fec9581faaa9042cb91f1d334dbd52332a795af
Sierra Laurent <sierraclaurent@gmail.com>
2024-02-26 09:05:33 -0800

    photo links fix

---
+++ .history/src/components/photoWriteUp_20240226090400.tsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { FiZoomIn } from 'react-icons/fi';
+
+const PhotoWriteUp: React.FC<{ imagePath: string; writeUp: string }> = ({ imagePath, writeUp }) => {
+  const [isBlurred, setIsBlurred] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  const handleClick = () => {
+    setIsBlurred(!isBlurred);
+  };
+
+  const toggleFullScreen = () => {
+    setIsFullScreen(!isFullScreen);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (!target.closest('.blur-image-container')) {
+        setIsBlurred(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className={`relative ${isFullScreen ? 'h-screen' : ''}`}>
+      <div className="relative blur-image-container">
+        <div className="absolute top-2 right-2 z-10">
+          <button onClick={toggleFullScreen}>
+            <FiZoomIn className={`text-black text-xl ${isFullScreen ? 'hidden' : ''}`} />
+          </button>
+        </div>
+        <Image
+          src={imagePath}
+          alt="BlurImage"
+          width={isFullScreen ? 1200 : 350}
+          height={isFullScreen ? 800 : 350}
+          className={`shadow-2xl rounded-lg ${isBlurred ? 'filter blur-md' : ''}`}
+          onClick={toggleFullScreen} // Changed to toggleFullScreen
+        />
+        {isBlurred && (
+          <div className="absolute inset-0 flex justify-center items-center bg-Emerald bg-opacity-75 rounded-lg">
+            <p className="text-white text-center p-4">{writeUp}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PhotoWriteUp;
